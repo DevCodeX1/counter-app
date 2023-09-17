76f26968dbaa8f6282e04e29aa9f91a544d769ce
DevCodeX <countryboya20@gmail.com>
2023-09-16 21:58:05 -0500

    final

---
+++ script.js
@@ -0,0 +1,18 @@
+// document.getElementById("count-el").innerText = 5;
+
+
+// intialixe the count 0
+// listen for clicks on the increment button
+// increment the count variable when the button is clicked
+// change the count-el in the HTML to reflect the new count 
+
+let count = 0;
+let countEl = document.getElementById("count-el")
+
+
+function increment(){
+    count = count + 1;
+    countEl.innerText = count 
+   
+    console.log(count)
+}
